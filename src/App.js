bad4676101344da739a3a34d442b3315087d3dc3
Carl Janz <carl.janz@gmail.com>
2020-03-06 11:41:15 -0800

    Improved buttons for health orb

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import "./App.css";
 import HealthOrb from "./Components/HealthOrb";
 import TechTree from "./Components/TechTree";
 import FakeData from "./FakeData.js";
+import styled from "styled-components";
+
+const Button = styled.span`
+  padding: 0 25px;
+  margin: 15px;
+  border: 1px white solid;
+  border-radius: 5px;
+`;
 
 class App extends React.Component {
   state = {
@@ -17,14 +25,7 @@ class App extends React.Component {
         <header className="App-header">
           <TechTree tech={FakeData} />
           <HealthOrb {...this.state} />
-          <span
-            onClick={event =>
-              this.setState({ health: Math.max(this.state.health - 30, 0) })
-            }
-          >
-            -
-          </span>
-          <span
+          <Button
             onClick={event =>
               this.setState({
                 health: Math.min(this.state.health + 10, this.state.maxHealth)
@@ -32,7 +33,14 @@ class App extends React.Component {
             }
           >
             +
-          </span>
+          </Button>
+          <Button
+            onClick={event =>
+              this.setState({ health: Math.max(this.state.health - 30, 0) })
+            }
+          >
+            -
+          </Button>
         </header>
       </div>
     );
